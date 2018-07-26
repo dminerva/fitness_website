56ad2a8f89ef53d60421008046546f22ed9e47aa
Daniel Minerva <dminerva94@yahoo.com>
2018-07-26 14:24:52 -0400

    working on tdee calc

--- js/tdee.js
+++ js/tdee.js
@@ -1,26 +1,51 @@
-function person(sex, heightF, heightI, weight, age, activity) {
+function convertHeight(feet, inch) {
+    var heightInInches = parseFloat((feet * 12) + inch);
+    var heightInCm = parseFloat(heightInInches * 2.54);
+
+    return heightInCm;
+}
+
+function convertWeight(lbs) {
+    var kilos = parseFloat(lbs * .454);
+
+    return kilos;
+}
+
+//takes in a person
+function calculateTdee(x) {
+    var bmr;
+    var tdee;
+
+    if(this.x == "male") {
+        bmr = 66 + (13.7 * x.weight) + (5 * x.height) - (6.8 * age);
+    } else if(this.x == "female") {
+        bmr = 655 + (9.6 * x.weight) + (1.8 * x.height) - (4.7 * x.age);
+    }
+
+    if(x.activity == "sedentary") {
+        tdee = bmr * 1.2;
+    } else if(x.activity == "light") {
+        tdee = bmr * 1.375;
+    } else if(x.activity == "moderate") {
+        tdee = bmr * 1.55;
+    } else if(x.activity == "very") {
+        tdee = bmr * 1.725;
+    }
+
+    return tdee;
+}
+
+function Person(sex, feet, inch, weight, age, activity) {
     this.sex = sex;
-    this.heightF = heightF;
-    this.heightI = heightI;
-    this.weight = weight;
+    this.height = convertHeight(feet, inch);
+    this.weight = convertWeight(weight);
     this.age = age;
     this.activity = activity;
 }
 
-function convertHeight(feet, inches) {
-    feet = parseFloat(feet);
-    inches = parseFloat(inches);
-    var feetInInch = 0;
-    var heightInCm;
-   
-    if(feet > 0 ) {
-        feetInInch = feet * 12;
-    }
-
-    if(feetInInch > 0) {
-        inches = feetInInch + inches;
-    }
-
-    heightInCm = inches * 2.54;
-    return heightInCm;
-}
+$(document).ready(function() {
+    $("#buttonP").click(function() {
+        var sex = $("#sexSelect option:selected").text();
+        
+    })
+})
